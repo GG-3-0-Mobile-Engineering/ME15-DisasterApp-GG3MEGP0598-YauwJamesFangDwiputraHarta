21b92d4408430f1ffbd91652ea268b8749b66556
James <jamesfangyauw@gmail.com>
2023-08-15 01:25:02 +0800

    create intrumentation test

---
+++ app/src/androidTest/java/com/james/disasterapp/MainActivityTest.kt
@@ -0,0 +1,43 @@
+package com.james.disasterapp
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @Before
+    fun start() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun checkFilterActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.fab_filter)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(FilterActivity::class.java.name))
+        Espresso.onView(ViewMatchers.withId(R.id.layout_province)).check(
+            ViewAssertions.matches(
+                ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.edt_province)).perform(ViewActions.click())
+    }
+
+    @Test
+    fun checkSettingActivity() {
+        Intents.init()
+        Espresso.onView(ViewMatchers.withId(R.id.fab_setting)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(SettingActivity::class.java.name))
+        Espresso.onView(ViewMatchers.withId(R.id.settings)).check(
+            ViewAssertions.matches(
+                ViewMatchers.isDisplayed()))
+    }
+}
